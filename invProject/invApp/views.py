be263b1a02c408327bbc2dd7d54186fa65c06301
Gbolahan <adesinagbolahan15@gmail.com>
2025-08-16 15:52:22 +0100

    Updated the views.py and the urls.py

--- invProject/invApp/views.py
+++ invProject/invApp/views.py
@@ -21,3 +21,22 @@ def product_create_view(request):
 def product_list_view(request):
     products = Product.objects.all()
     return render(request, 'invApp/product_list.html', {'products':products})
+
+# Update View
+def product_update_view(request, product_id):
+    product = Product.objects.get(product_id=product_id)
+    form = ProductForm(instance=product) #Incase error dey, comot wetin dey inside the bracket
+    if request.method == "POST":
+        ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    return render(request, 'invApp/product_form.html', {'form':form})
+
+# Delete View
+def product_delete_view(request, product_id):
+    product = Product.objects.get(product_id=product_id)
+    if request.method == "POST":
+        product.delete()
+        return redirect('product_list')
+    return render(request, 'invApp/product_confirm_delete.html', {'product':product})
